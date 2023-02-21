adf4020090e973d47ffa313eefda04df09632c29
lpkll <lpkll123@gmail.com>
2023-02-21 22:44:07 +0700

    Added Crucifix Item

--- src/main/java/net/namozdizex/kisane/registry/KMItems.java
+++ src/main/java/net/namozdizex/kisane/registry/KMItems.java
@@ -9,10 +9,12 @@ import net.namozdizex.kisane.KisaneMikoMod;
 
 public class KMItems {
     public static final Item KISANE_MIKO_SPAWN_EGG = new SpawnEggItem(KMEntity.KISANE_MIKO, 0x1cb704, 0xfb0407, new Item.Properties().tab(CreativeModeTab.TAB_MISC));
+    public static final Item CRUCIFIX = new Item(new Item.Properties().tab(KisaneMikoMod.TAB).stacksTo(1));
 
     public static void init()
     {
         register("kisane_miko_spawn_egg", KISANE_MIKO_SPAWN_EGG);
+        register("crucifix", CRUCIFIX);
     }
     private static void register(String key, Item item)
     {

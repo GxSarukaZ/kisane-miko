79425b6d515850b61ebba183a71e476cdd55fe6e
lpkll <lpkll123@gmail.com>
2023-02-22 18:07:09 +0700

    Add Crucifix and Crucifix Advancement

--- src/main/java/net/namozdizex/kisane/registry/KMItems.java
+++ src/main/java/net/namozdizex/kisane/registry/KMItems.java
@@ -9,12 +9,28 @@ import net.namozdizex.kisane.KisaneMikoMod;
 
 public class KMItems {
     public static final Item KISANE_MIKO_SPAWN_EGG = new SpawnEggItem(KMEntity.KISANE_MIKO, 0x1cb704, 0xfb0407, new Item.Properties().tab(CreativeModeTab.TAB_MISC));
-    public static final Item CRUCIFIX = new Item(new Item.Properties().tab(KisaneMikoMod.TAB).stacksTo(1));
+
+    public static final Item NORMAL_CRUCIFIX = new Item(new Item.Properties().tab(KisaneMikoMod.TAB).stacksTo(1));
+    public static final Item TRUE_CRUCIFIX = new Item(new Item.Properties().tab(KisaneMikoMod.TAB));
+    public static final Item BLOODY_CRUCIFIX = new Item(new Item.Properties().tab(KisaneMikoMod.TAB));
+    public static final Item CRUCIFIX_SHARD = new Item(new Item.Properties().tab(KisaneMikoMod.TAB));
+    public static final Item CRUCIFIX_SHARD_1 = new Item(new Item.Properties().tab(KisaneMikoMod.TAB));
+    public static final Item CRUCIFIX_SHARD_2 = new Item(new Item.Properties().tab(KisaneMikoMod.TAB));
+    public static final Item CRUCIFIX_SHARD_3 = new Item(new Item.Properties().tab(KisaneMikoMod.TAB));
+    public static final Item CRUCIFIX_SHARD_4 = new Item(new Item.Properties().tab(KisaneMikoMod.TAB));
 
     public static void init()
     {
         register("kisane_miko_spawn_egg", KISANE_MIKO_SPAWN_EGG);
-        register("crucifix", CRUCIFIX);
+
+        register("normal_crucifix", NORMAL_CRUCIFIX);
+        register("true_crucifix", TRUE_CRUCIFIX);
+        register("bloody_crucifix", BLOODY_CRUCIFIX);
+        register("crucifix_shard", CRUCIFIX_SHARD);
+        register("crucifix_shard1", CRUCIFIX_SHARD_1);
+        register("crucifix_shard2", CRUCIFIX_SHARD_2);
+        register("crucifix_shard3", CRUCIFIX_SHARD_3);
+        register("crucifix_shard4", CRUCIFIX_SHARD_4);
     }
     private static void register(String key, Item item)
     {
